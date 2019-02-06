ef12f7888417bb0ac29e99eb0fd123ef9d43d3b6
Olcay Bayram <olcaybayram@gmail.com>
2019-02-06 23:16:54 +0100

    Leap motion reference is added

--- LeapSample/Program.cs
+++ LeapSample/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Leap;
 
 namespace LeapSample
 {
@@ -10,12 +7,16 @@ namespace LeapSample
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            SampleListener listener = new SampleListener();
+            Controller controller = new Controller();
+            controller.AddListener(listener);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Keep this process running until Enter is pressed
+            Console.WriteLine("Press Enter to quit...");
+            Console.ReadLine();
+
+            controller.RemoveListener(listener);
+            controller.Dispose();
         }
     }
 }
---
+++ LeapSample/SampleListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Leap;
+
+namespace LeapSample
+{
+    class SampleListener : Listener
+    {
+        private Object thisLock = new Object();
+
+        private void SafeWriteLine(String line)
+        {
+            lock (thisLock)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public override void OnConnect(Controller controller)
+        {
+            SafeWriteLine("Connected");
+            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+        }
+
+
+        public override void OnFrame(Controller controller)
+        {
+            Frame frame = controller.Frame();
+
+            SafeWriteLine("Frame id: " + frame.Id
+                                       + ", timestamp: " + frame.Timestamp
+                                       + ", hands: " + frame.Hands.Count
+                                       + ", fingers: " + frame.Fingers.Count
+                                       + ", tools: " + frame.Tools.Count
+                                       + ", gestures: " + frame.Gestures().Count);
+        }
+    }
+}
